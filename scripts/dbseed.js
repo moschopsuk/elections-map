77023cc5c858fa2152c64973576eb60d74a9e186
Luke Moscrop <luke.moscrop@bbc.co.uk>
2017-04-21 20:06:33 +0100

    eslinting

--- scripts/dbseed.js
+++ scripts/dbseed.js
@@ -1,3 +1,9 @@
+/*
+   This is a script that has to report back
+   to the cli so we will allow console logs
+*/
+/* eslint-disable no-console */
+
 const fs = require('fs');
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
@@ -8,64 +14,64 @@ const Result = require('../src/models/Results');
 mongoose.Promise = Promise;
 
 const csv = fs.readFileSync(`${__dirname}/sources/hocl-ge2015-results-summary.csv`, 'utf8');
-const results = parse(csv, {columns: true });
+const results = parse(csv, { columns: true });
 
-mongoose.connect('mongodb://localhost:32768/elections', async function(err) {
+mongoose.connect('mongodb://localhost:32768/elections', async (err) => {
   if (err) {
     throw err;
   }
+
   console.log(chalk.blue('Starting Ingest'));
   const ingestJobs = [];
 
-  //Purge Existing
+  // Purge Existing
   await Result.remove({}).exec();
 
   results.forEach((data) => {
     const constituency = new Result({
-      constituency:   data.ons_id,
-      year:   2015,
+      constituency: data.ons_id,
+      year: 2015,
       geography: {
-        name:   data.constituency_name,
+        name: data.constituency_name,
         county: data.county_name,
         region: data.region_name,
-        type:   data.constituency_type
+        type: data.constituency_type,
       },
       winningPartyCode: data.first_party,
-      decleared:        data.declaration_time,
+      decleared: data.declaration_time,
       ballot: {
         electorate: data.electorate,
-        valid:      data.valid_votes,
-        invalid:    data.invalid_votes,
-        majority:   data.majority
+        valid: data.valid_votes,
+        invalid: data.invalid_votes,
+        majority: data.majority,
       },
       parties: {
-        con:      data.con,
-        lab:      data.lab,
-        ld:       data.ld,
-        ukip:     data.ukip,
-        green:    data.green,
-        snp:      data.snp,
-        pc:       data.pc,
-        dup:      data.dup,
-        sf:       data.sf,
-        sdlp:     data.sdlp,
-        uup:      data.uup,
+        con: data.con,
+        lab: data.lab,
+        ld: data.ld,
+        ukip: data.ukip,
+        green: data.green,
+        snp: data.snp,
+        pc: data.pc,
+        dup: data.dup,
+        sf: data.sf,
+        sdlp: data.sdlp,
+        uup: data.uup,
         alliance: data.alliance,
-        other:    data.other
-      }
+        other: data.other,
+      },
     });
 
-    ingestJobs.push(constituency.save())
+    ingestJobs.push(constituency.save());
   });
 
   try {
-    const jobresult = await Promise.all(ingestJobs)
+    const jobresult = await Promise.all(ingestJobs);
     console.log(chalk.dim(`${jobresult.length} parsed...`));
     console.log(chalk.bold.green('Ingest Complete!'));
     mongoose.disconnect();
     process.exit(0);
-  }
-  catch (e) {
+  } catch (e) {
     console.log(chalk.red(e));
   }
 });
--- src/models/Results.js
+++ src/models/Results.js
@@ -1,39 +1,40 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 // TODO: Think of a better model name
-var ResultSchema = new Schema({
-    constituency: { type: String, index: true, required: true },
-    year: { type: Number, required: true },
-    geography: {
-      name: { type: String },
-      county: { type: String },
-      region: { type: String },
-      type: { type: String }
-    },
-    winningPartyCode: { type: String, required: true },
-    decleared: { type: Date },
-    ballot: {
-      electorate: { type: Number },
-      valid: { type: Number },
-      invalid: { type: Number },
-      majority: { type: Number }
-    },
-    parties: {
-      con: { type: Number },
-      lab: { type: Number },
-      ld: { type: Number },
-      ukip: { type: Number },
-      green: { type: Number },
-      snp: { type: Number },
-      pc: { type: Number },
-      dup: { type: Number },
-      sf: { type: Number },
-      sdlp: { type: Number },
-      uup: { type: Number },
-      alliance: { type: Number },
-      other: { type: Number }
-    }
+const ResultSchema = new Schema({
+  constituency: { type: String, index: true, required: true },
+  year: { type: Number, required: true },
+  geography: {
+    name: { type: String },
+    county: { type: String },
+    region: { type: String },
+    type: { type: String },
+  },
+  winningPartyCode: { type: String, required: true },
+  decleared: { type: Date },
+  ballot: {
+    electorate: { type: Number },
+    valid: { type: Number },
+    invalid: { type: Number },
+    majority: { type: Number },
+  },
+  parties: {
+    con: { type: Number },
+    lab: { type: Number },
+    ld: { type: Number },
+    ukip: { type: Number },
+    green: { type: Number },
+    snp: { type: Number },
+    pc: { type: Number },
+    dup: { type: Number },
+    sf: { type: Number },
+    sdlp: { type: Number },
+    uup: { type: Number },
+    alliance: { type: Number },
+    other: { type: Number },
+  },
 });
 
 module.exports = mongoose.model('Result', ResultSchema);
