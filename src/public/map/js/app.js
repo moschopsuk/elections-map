caa0732cd0919d1f0fb2624b4b7081efbfc62fe5
Luke Moscrop <luke.moscrop@bbc.co.uk>
2017-04-24 18:36:10 +0100

    Added socket.io

--- src/public/map/js/app.js
+++ src/public/map/js/app.js
@@ -5,7 +5,8 @@ requirejs.config({
         'jquery': './vendors/jquery/jquery-2.0.3',
         'backbone': './vendors/backbone/backbone',
         'underscore': './vendors/backbone/underscore',
-        'd3': 'lib/vendors/d3/d3.min'
+        'd3': './vendors/d3/d3.min',
+        'socket.io': '/socket.io/socket.io'
     }
 });
 
--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const http = require('http');
+const socket = require('socket.io');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
+const server = http.Server(app);
+const io = socket(server);
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
+server.listen(port, () => {
   mongoose.connect('mongodb://localhost:32768/elections', { server: { socketOptions: { keepAlive: 1 } } });
   console.log(`http://127.0.0.1:${port}`);
+});
+
+io.on('connection', () => {
+
 });
