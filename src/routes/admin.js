13d1f5b3eb0f766e52d8b73a96497fba309b04ac
Luke Moscrop <luke.moscrop@bbc.co.uk>
2017-04-28 08:18:46 +0100

    Redirect on no letter, better error handling

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,10 +8,18 @@ module.exports = () => {
     const constituency = req.body.constituency;
     const winningPartyCode = req.body.partyCode;
     const letter = req.params.letter;
-
-    await LiveResults.update({ constituency }, { $set: { winningPartyCode } });
+    try {
+      await LiveResults.update({ constituency }, { $set: { winningPartyCode } });
+    } catch (e) {
+      req.flash('error', e);
+      res.redirect(`/admin/${letter}`);
+    }
     req.flash('success', `Updated ${constituency} to ${winningPartyCode}`);
     res.redirect(`/admin/${letter}`);
+  });
+
+  router.get('/', (req, res) => {
+    res.redirect('/admin/A');
   });
 
   router.get('/:letter', async (req, res) => {
